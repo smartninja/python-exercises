b002cbbc6e5b4dfeee3e2dcd6120145731c8dcc6
Matej Ramuta <matej.ramuta@gmail.com>
2016-07-13 15:40:31 +0200

    todo example updated

--- lesson3/todo.py
+++ lesson3/todo.py
@@ -19,14 +19,24 @@ while True:
 
 print("All tasks: %s" % todo_dict)
 
+todo_file = open("todo.txt", "w+")  # open the TXT file (or create a new one)
+
 print("Completed tasks:")
+todo_file.write("Completed tasks:\n")  # write into the TXT file
 for item in todo_dict:
     if todo_dict[item] is True:
         print("- " + item)
+        todo_file.write("- " + item + "\n")  # add task into the TXT file
+
+todo_file.write("\n")
 
 print("Incomplete tasks:")
+todo_file.write("Incomplete tasks:\n")  # write into the TXT file
 for item in todo_dict:
     if todo_dict[item] is False:
         print("- " + item)
+        todo_file.write("- " + item + "\n")  # add task into the TXT file
+
+todo_file.close()  # close the TXT file
 
 print("END")
