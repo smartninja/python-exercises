e3fc45e2194b9ccb1ecf5bebb5bf44800b6673c9
Matej Ramuta <matej.ramuta@gmail.com>
2016-07-31 12:33:21 +0200

    different way of getting items from dict

--- lesson3/todo.py
+++ lesson3/todo.py
@@ -24,7 +24,7 @@ todo_file = open("todo.txt", "w+")  # open the TXT file (or create a new one)
 print("Completed tasks:")
 todo_file.write("Completed tasks:\n")  # write into the TXT file
 for item in todo_dict:
-    if todo_dict[item] is True:
+    if todo_dict.get(item) is True:
         print("- " + item)
         todo_file.write("- " + item + "\n")  # add task into the TXT file
 
@@ -33,7 +33,7 @@ todo_file.write("\n")
 print("Incomplete tasks:")
 todo_file.write("Incomplete tasks:\n")  # write into the TXT file
 for item in todo_dict:
-    if todo_dict[item] is False:
+    if todo_dict.get(item) is False:
         print("- " + item)
         todo_file.write("- " + item + "\n")  # add task into the TXT file
 
